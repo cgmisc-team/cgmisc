9216a4f2c76eeabc28d870e3b85a6af0c360fbb3
simfor <simon@madartwork.com>
2015-03-12 18:46:44 +0100

    Improvements to boxplot.snp functions Renamed the function SNP.box.twoWay to boxplot.snp.twoWay. Also made some graphical changes and removed a number of functionalities and their respective arguments.

--- R/boxplot.snp.R
+++ R/boxplot.snp.R
@@ -4,7 +4,7 @@
 ##' Works for both outbreed (three boxes) and inbreed (two boxes) data. 
 ##'@author Simon Forsberg <\email{simon.forsberg@@slu.se}>
 ##'@param GWASdata a gwaa.data class object as used by \code{\link[GenABEL]{gwaa.data-class}}
-##'@param marker SNP name
+##'@param marker SNP name or index
 ##'@param trait trait name
 ##'@param recode if genotypes are to be recoded to 'AA','Aa','aa'.
 ##'@return null
@@ -20,9 +20,8 @@ boxplot.snp  <- function(data, marker, trait, recode = F, ...){
   }
   y <- phdata(data)[,trait]
   max.y <- max(y, na.rm=T)
-  box <- boxplot(y ~ as.matrix(geno), col = 'lightblue', las = 1, frame=F, cex.axis = .8,  ylim = range(pretty(c(0, max.y))))
+  box <- boxplot(y ~ as.matrix(geno), col = 'lightblue', las = 1, frame=F, cex.axis = .8,  ylim = range(pretty(c(0, max.y))), ...)
   grid()
-  boxplot(y ~ as.matrix(geno), col = 'lightblue', las = 1, frame=F, cex.axis = .8,  ylim = range(pretty(c(0, max.y))), add = T)
   axis(1, at=1:length(box$n), labels=paste("n =", box$n), line=2, lty=0)
   axis(1, labels=FALSE)
   genotypes <- as.character(unique(geno[,1]))
---
+++ R/boxplot.snp.twoWay.R
@@ -0,0 +1,38 @@
+##'@title SNP_box_twoWay
+##'@description Same as boxplot.snp but based on two SNPs. 
+##'This function makes box plots, showing the Genotype x Phenotype map for two loci. 
+##'Works for both outbreed (nine boxes) and inbreed (four boxes) data. 
+##'Can be used to look for epistasis.
+##'
+##'@param data a gwaa.data class object as used by \code{\link[GenABEL]{gwaa.data-class}}
+##'@param marker1 The name or index of one marker/SNP in data
+##'@param marker2 The name or index of a second marker/SNP in data
+##'@param trait The name of a phenotype in data
+##'@return null
+##'@author Simon Forsberg  <\email{simon.forsberg@slu.se}>
+##'
+boxplot.snp.twoWay <- function(data, marker1, marker2, trait, ...){
+  marker1.geno <- as.genotype.gwaa.data(data[,marker1])
+  marker2.geno <- as.genotype.gwaa.data(data[,marker2])
+  
+  y <- phdata(data)[,trait]
+  box <- boxplot(y ~ as.matrix(marker1.geno)*as.matrix(marker2.geno), col = 'lightblue', las = 1, frame=F, cex.axis = .8, xaxt = "n", ...)
+  grid()
+  axis(1, at=1:length(box$n), labels=paste("n =", box$n), line=2, lty=0)
+  
+  labels.marker1 <- gsub(pattern = "(.*)\\..*", replacement = "\\1", x = box$names)
+  labels.marker2 <- gsub(pattern = ".*\\.(.*)", replacement = "\\1", x = box$names)
+  labels.marker2.nrPerClass <- table(labels.marker2)[1]
+  tmp <- 1 + (labels.marker2.nrPerClass - 1)/2
+  tmp2 <- sapply(X = 2:length(unique(labels.marker2)) - 1, FUN = function(x){tmp + x*labels.marker2.nrPerClass })
+  labels.marker2.at <- c(tmp, tmp2)
+  axis(1, at = 1:length(labels.marker1), labels = labels.marker1, cex.axis = .8)
+  axis(3, at = labels.marker2.at, labels=unique(labels.marker2), cex.axis = .8)
+  
+  labels.marker2.table <- table(labels.marker2)
+  j <- labels.marker2.table[1]
+  for(i in 2:length(labels.marker2.table)){
+    abline(v = j + .5)
+    j <- j + labels.marker2.table[1]
+  }
+}
