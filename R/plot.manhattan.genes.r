d79e9d37da6c918a36122d5d0eecf1110379cfa9
Veronika Scholz <veronikascholz@gmail.com>
2015-05-28 23:45:57 +0200

    Minor changes in manhattan gene plot

--- R/plot.manhattan.genes.r
+++ R/plot.manhattan.genes.r
@@ -45,6 +45,7 @@ plot.manhattan.genes <- function(data, gwas.result, chr, region, index.snp, p.va
     }
   }
   
+  # Get the minor allele frequency for each SNP
   getMAF <- function(data, region) {
     summ <- summary(data[,region])
     tmp_maf <- (2 * summ$P.22 + summ$P.12) / (2 * summ$NoMeasured)
@@ -57,6 +58,7 @@ plot.manhattan.genes <- function(data, gwas.result, chr, region, index.snp, p.va
 
   mafThreshold <<- mafThreshold
   
+  # Get the markers in the specified region and categorize them according to their p-value
   startCoord <- region[1]
   stopCoord  <- region[2]
   myChromosome <- data@gtdata[ ,which(data@gtdata@chromosome == chr)]
@@ -65,8 +67,6 @@ plot.manhattan.genes <- function(data, gwas.result, chr, region, index.snp, p.va
   r2matrix[lower.tri(r2matrix)] <- t(r2matrix)[lower.tri(r2matrix)]
   markers <- which(data@gtdata@snpnames %in% names(area)) 
   markers.coords <<- data@gtdata@map[markers]
-  idx.marker <- which(data@gtdata@snpnames == index.snp)
-  idx.marker.coords <- data@gtdata@map[idx.marker]
   pvals <- -log10(gwas.result@results$P1df[markers])
   
   r2vec <- r2matrix[index.snp,]
@@ -74,20 +74,33 @@ plot.manhattan.genes <- function(data, gwas.result, chr, region, index.snp, p.va
   r2col <<- cut(r2vec, breaks=c(1.0,0.8,0.6,0.4,0.2,0.0,-1), 
                labels=rev(c("(0.8-1.0]","(0.6-0.8]", "(0.4-0.6]", "(0.2-0.4]", "[0.0-0.2]", "INDEX")), include.lowest=T)
   
-  df2 <- data.frame(markers = markers.coords, pvals = pvals)
+  df2 <<- data.frame(markers = markers.coords, pvals = pvals)
   maf <- getMAF(myChromosome, area)
   maf2 <<- pmin(maf$maf, (1 - maf$maf))
-  gwas_palette <- rev(c("#9E0508","tomato", "chartreuse3", "cyan3","navy","black" ))
+  gwas_palette <- rev(c("#9E0508","tomato","chartreuse3","cyan3","navy","black"))
+  
+  # First plot the manhattan plot
   manhattan_plot <- ggplot(df2, aes(x=markers.coords, y=pvals, colour = r2col)) +
     coord_cartesian(xlim = region) +
-    scale_colour_manual(values=gwas_palette, name=expression(r^2)) +
+    scale_colour_manual(name=expression(r^2),
+                        values=gwas_palette,
+                        labels = c("INDEX","[0.0-0.2]", "(0.2-0.4]","(0.4-0.6]","(0.6-0.8]","(0.8-1.0]")) +
+    scale_shape_manual(name = expression(r^2),
+                       values=gwas_palette,
+                       labels = c(112, 15, 15, 15, 15, 15)) +
     geom_point(size=2) +
+    geom_point(data=df2[index.snp,], aes(x=df2[index.snp,1], y=df2[index.snp,2]),
+               colour="black", shape=21, fill="white", size=2, guide = 'legend') +
     ylab(expression(-log[10](p-value))) +
     theme_bw() +
-    theme(plot.margin=unit(c(5,7,0,5),"mm"), legend.position=c(0.9,0.8), panel.border=element_blank()) +
+    theme(plot.margin=unit(c(5,7,0,5),"mm"), legend.position=c(0.9,0.85),
+          legend.key=element_blank(), panel.border=element_blank(),
+          legend.key.height = unit(4, "mm")) + 
     theme(axis.line = element_line(color = 'grey'), axis.text.x=element_blank(),
           axis.title.x=element_blank())
+
   
+  # Second plot showing the minor allele frequency
   maf_plot <- ggplot(df2, aes(x=markers.coords, y=(maf2))) +
     coord_cartesian(xlim = region) +
     geom_line(color = "#2C7FB8") +
@@ -95,10 +108,11 @@ plot.manhattan.genes <- function(data, gwas.result, chr, region, index.snp, p.va
     geom_hline(aes(yintercept=mafThreshold), color = "tomato") +
     theme_bw() +
     theme(plot.margin=unit(c(0,7,0,4),"mm"), legend.position="none", panel.border=element_blank(),
-          legend.text=element_text(size=3), legend.background = element_blank()) +
+          legend.text=element_text(size=2), legend.background = element_blank()) +
     theme(axis.line = element_line(color = 'grey'), axis.text.x=element_blank(),
           axis.title.x=element_blank())
   
+  # Third plot showing the genes in the specified region
   gene_plot <- plot.genes(region, chr, bed.path=bed.path)
   
   layOut(list(manhattan_plot, 1:4, 1),
