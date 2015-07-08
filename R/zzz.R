c67ff9f39c50e8f5d92d6846fdf8727915f5b3fa
wildberry93 <jagoda100jablonska@gmail.com>
2015-07-08 22:14:43 +0200

    warning messages

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 #Displays a msg 
+options(warn=-1)
 
 .onAttach <- function(lib,pkg = "cgmisc") {
   packageStartupMessage("\n Package cgmisc contains miscellaneous functions, useful for extending
@@ -16,7 +17,7 @@ genome-wide association study (GWAS) analyses. \n")
                             "\n","Version:", Version,
                             "\n", "Date:",Date,
                             "\n","Author:",Author,
-                            "\n","License",License,"\n\n", Title))
+                            "\n","License",License,"\n\n", Title,"\n"))
   
   #install Bioconductor packages if necessary
   
@@ -29,5 +30,18 @@ genome-wide association study (GWAS) analyses. \n")
   if(!require(rtracklayer)){
     biocLite("rtracklayer")
   }
+  
+  #chceck versions of dependencies
+  
+  packages <- c(GenABEL='1.7.4', ggplot2='1.0.1', grid='3.1.1', genetics='1.3.8.1')
+  inst.pkgs <- installed.packages()[,3]
+  installed <- sort(inst.pkgs[which(names(inst.pkgs) %in% names(packages))])
+  required <- sort(packages[names(installed)])
+  a <- installed[which(sort(installed)!=sort(required))]
+  b <- required[which(sort(installed)!=sort(required))]
 
+  packageStartupMessage(
+  if(length(a)!=0){
+    paste("\nWARNING! The following package version differs from the recommendend one:", names(a),a, "->", names(b),b,"\n")
+  })
 }
